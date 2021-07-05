da6d8ce7dda6322859a42c9e6f3e3fd5e360b413
kaihr <lyhkhai@gmail.com>
2021-07-05 22:10:08 +0700

    - feature add: extract sentences relate to the given keyword

--- SimpleSearchEngine.h
+++ SimpleSearchEngine.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <stdio.h>
 
 int isFirstRun();
 bool init();
@@ -24,3 +25,5 @@ void find100Best(wchar_t** pathList, int nFiles, const wchar_t* keywords);
 
 bool addFile(int& nFiles, const wchar_t* filePath, const wchar_t* pathPath, wchar_t**& pathList, wchar_t** stopWords, int nStopWords);
 bool remFile(int nFiles, const wchar_t* filePath, const wchar_t *pathPath, wchar_t** pathList, wchar_t** stopWords, int nStopWord);
+
+void extractResults(FILE* fout, const wchar_t* filePath, const wchar_t* metaDataPath, wchar_t** keyword, int nWords);
---
+++ StringMatching.h
@@ -0,0 +1,2 @@
+#pragma once
+bool isMatched(const wchar_t* haystack, const wchar_t* needle);
